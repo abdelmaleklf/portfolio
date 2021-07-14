5890361575ccda1541bd90d92018436299d56b46
Abdelmalek <abdelmalek.lf@outlook.com>
2021-07-14 13:11:16 +0200

    Portfolio first commit

---
+++ portfolio/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from portfolio.models import Info, Social, Testimonials, Project
+
+# Register your models here.
+admin.site.register(Info)
+admin.site.register(Social)
+admin.site.register(Testimonials)
+admin.site.register(Project)
---
+++ portfolio/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Info(models.Model):
+    birthday = models.DateField(blank=False)
+    phone = models.CharField(blank=False, max_length=50)
+    email = models.EmailField(blank=False, max_length=254)
+    degree = models.CharField(blank=False, max_length=50)
+    city = models.CharField(blank=False, max_length=50)
+    website = models.URLField(blank=False, max_length=200)
+
+
+class Social(models.Model):
+    github = models.URLField(blank=False, max_length=200)
+    linkedin = models.URLField(blank=False, max_length=200)
+
+
+class Testimonials(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+    comment = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+    url = models.URLField(blank=True, max_length=200)
+    image = models.ImageField(upload_to='static/images/')
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
