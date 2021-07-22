50c04b76a438e60ccd47abd335319118b2454941
Abdelmalek <abdelmalek.lf@outlook.com>
2021-07-22 11:25:27 +0200

    update index.html & skills to admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from portfolio.models import Info, Social, Testimonial, Project
+from portfolio.models import Info, Social, Testimonial, Project, Skill
 
 # Register your models here.
 admin.site.register(Info)
 admin.site.register(Social)
 admin.site.register(Testimonial)
 admin.site.register(Project)
+admin.site.register(Skill)
